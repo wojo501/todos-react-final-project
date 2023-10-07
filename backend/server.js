16f5df66c4e177440d910d1f36b40b91b13f8ff4
Wojciech Michaluk <wojo.michaluk@gmail.com>
2023-10-07 03:38:31 +0200

    start

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/app", routesUrls);
 
-const port = process.env.PORT || 5000; // Use the PORT environment variable if set, or fallback to 5000
+const port = process.env.PORT || 3000; // Use the PORT environment variable if set, or fallback to 5000
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-export default app;
